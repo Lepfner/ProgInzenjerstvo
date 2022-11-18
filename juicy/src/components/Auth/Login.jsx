a2f7d570daf2a51f11ff471e8a12be05b20e2a6d
ToniGrbic <tonigrbic.5@gmail.com>
2022-11-18 13:46:07 +0100

    popravljena responzivnost za login component

--- juicy/src/components/Auth/Login.jsx
+++ juicy/src/components/Auth/Login.jsx
@@ -1,44 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmail("");
+    setPassword("");
+  };
   return (
-    <div className="w-5/6 flex flex-col justify-center pl-8 pb-12 max-sm:pl-2 pb-8">
+    <div className=" flex flex-col justify-center pl-8 pb-12 lg:w-5/6 md:w-full max-sm:pl-0 pb-8 w-full">
       <h1 className="lg:text-6xl mb-2 md: text-5xl sm: text-4xl">LOGIN:</h1>
       <div className="w-full flex flex-col lg:text-lg md:flex-row text-base sm:flex-col ">
-        <form className="lg: w-4/5 max-md:w-full">
+        <form className="lg: w-4/5 max-md:w-full" onSubmit={handleSubmit}>
           <p>E-mail:</p>
           <input
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             type="email"
-            className="h-12 rounded-lg bg-gray-300 mb-8 w-full lg:w-4/5 md:w-4/5"
+            className={`${
+              email !== "" ? `validate` : ""
+            } h-14 px-2 rounded-lg bg-gray-300 mb-8 w-full lg:w-4/5 md:w-4/5`}
           />
           <p>Password:</p>
           <input
+            required
+            value={password}
+            minLength={7}
+            onChange={(e) => setPassword(e.target.value)}
             type="password"
-            className="h-12 rounded-lg bg-gray-300 mb-8 w-full lg:w-4/5 md:w-4/5"
+            className={`${
+              password !== "" ? `validate` : ""
+            } h-14 px-2 rounded-lg bg-gray-300 mb-8 w-full lg:w-4/5 md:w-4/5`}
           />
-          <div className="flex lg:gap-8 sm: gap-4 ">
+          <div className="flex  lg:gap-8 flex-row md:flex-row gap-2 max-sm:flex-col ">
             <button
               type="submit"
-              className="block bg-orange-500 rounded-md p-2 mt-4 w-24 text-white"
+              className="block bg-orange-500 px-4 rounded-md p-2 mt-4 text-white hover:bg-orange-600"
             >
               LOGIN
             </button>
             <button
               type="submit"
-              className="block bg-orange-500 rounded-md p-2 min-w-20 mt-4 text-white"
+              className="block bg-orange-500 px-4 rounded-md p-2 mt-4 text-white hover:bg-orange-600"
             >
-              REGISTER
+              SIGN-UP
             </button>
           </div>
         </form>
         <div
-          className=" w-4/5 border-solid border-2 pl-4 mt-4 border-l-orange-500 
-                         lg:h-40 md:w-2/5 h-40 pl-8 max-sm:h-auto"
+          className=" border-solid border-2 pl-4 mt-4 border-l-orange-500 
+                         lg:h-40 md:w-2/5 pl-8 max-sm:h-auto"
         >
-          <p className="text mb-4">Login using e-mail and password</p>
+          <p className="text mb-4 font-bold">Login using e-mail and password</p>
 
           <p>
-            <Link to="/Recovery">forgot password?</Link>
+            <Link to="/Recovery font-bold">Forgot password?</Link>
           </p>
         </div>
       </div>
