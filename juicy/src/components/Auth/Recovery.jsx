70ee77bfdbc418a188bfd2ec83ac2ce221becc40
Jozo Krstanović <jozo.molten@gmail.com>
2022-11-19 15:49:59 +0100

    Zavrsena Recovery komponenta

--- juicy/src/components/Auth/Recovery.jsx
+++ juicy/src/components/Auth/Recovery.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Recovery = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmail("");
+  }
+
+
   return (
-    <div>Recovery</div>
+    <div className=" flex flex-col justify-center pl-8 pb-12 lg:w-5/6 md:w-full max-sm:pl-0 pb-8 w-full">
+      <h1 className="lg:text-6xl mb-2 md: text-5xl sm: text-4xl">RECOVERY:</h1>
+      <div className="w-full flex flex-col lg:text-lg md:flex-row text-base sm:flex-col ">
+        <form className="lg: w-4/5 max-md:w-full" onSubmit={handleSubmit}>
+          <p>E-mail:</p>
+          <input
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            className={`${email !== "" ? `validate` : ""
+              } h-14 px-2 rounded-lg bg-gray-300 mb-8 w-full lg:w-4/5 md:w-4/5`}
+          />
+          <div className="flex  lg:gap-8 flex-row md:flex-row gap-2 max-sm:flex-col ">
+            <button
+              type="submit"
+              className="block bg-orange-500 px-4 rounded-md p-2 mt-4 text-white hover:bg-orange-600"
+            >
+              SEND
+            </button>
+            <button
+              type="submit"
+              className="block bg-orange-500 px-4 rounded-md p-2 mt-4 text-white hover:bg-orange-600"
+            >
+              BACK
+            </button>
+          </div>
+        </form>
+        <div
+          className=" border-solid border-2 pl-4 mt-4 border-l-orange-500 
+                         lg:h-40 md:w-2/5 pl-8 max-sm:h-auto"
+        >
+          <p className="text mb-4 font-bold">Write your e-mail so we can
+            send you your information.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
