fad27b31e216be85abd6aa8f665f6391c0d78f6a
Lepfner <andrija@blissproductions.io>
2022-12-03 11:43:31 +0100

    Added 404 page, added another route for login page

--- juicy/src/App.js
+++ juicy/src/App.js
@@ -1,8 +1,9 @@
 import "./Styles/App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import AuthLayout from "./components/Auth/AuthLayout.jsx";
-import Dashboard from "./components/Dashboard/MainMenu"
+import Dashboard from "./components/dashboard/MainMenu"
 import ProfileSetup from "./components/profileSetup/profileSetup";
+import ErrorPage from "./components/404";
 import {
   Login,
   SignUp,
@@ -16,11 +17,13 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<AuthLayout />}>
+          <Route path="/" element={<Login />}/>
           <Route path="/Login" element={<Login />}/>
           <Route path="/AdminLogin" element={<AdminLogin/>}/>
           <Route path="/SignUp" element={<SignUp />}/>
           <Route path="/Confirmation" element={<Confirmation />}/>
           <Route path="/Recovery" element={<Recovery />}/>
+          <Route path='*' element={<ErrorPage />}/>
         </Route>
         <Route path="/Dashboard" element={<Dashboard/>}/>
         <Route path="/Setup" element={<ProfileSetup/>}/>
