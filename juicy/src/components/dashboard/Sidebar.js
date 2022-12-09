f79eb9ba31e9f137bf6e9d021c4c4c6c6c457422
ToniGrbic <tonigrbic.5@gmail.com>
2022-12-09 15:59:31 +0100

    postavljen max-width na sidebar

--- juicy/src/components/dashboard/Sidebar.js
+++ juicy/src/components/dashboard/Sidebar.js
@@ -1,19 +1,41 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faBars, faSquareCaretDown } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faBars,
+  faSquareCaretDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Sidebar() {
-    return (
-        <div className='bg-orange-500 w-full h-1/2 flex min-h-[15rem] flex-col justify-around items-center border-solid rounded-r-3xl'>
-            <div>
-            <FontAwesomeIcon id="logoIcon" className='hover:animate-pulse' icon={faBars} size="2x"/>
-            </div>
-            <div>
-            <FontAwesomeIcon id="logoIcon" className='hover:animate-pulse' icon={faUser} size="2x"/>
-            </div>
-            <div>
-            <FontAwesomeIcon id="logoIcon" className='hover:animate-pulse' icon={faSquareCaretDown} size="2x"/>
-            </div>
-        </div>
-    )
+  return (
+    <div
+      className="bg-orange-500 max-w-[6rem] w-full h-1/2 flex min-h-[15rem] 
+                      flex-col justify-around items-center border-solid rounded-r-3xl"
+    >
+      <div>
+        <FontAwesomeIcon
+          id="logoIcon"
+          className="hover:animate-pulse"
+          icon={faBars}
+          size="2x"
+        />
+      </div>
+      <div>
+        <FontAwesomeIcon
+          id="logoIcon"
+          className="hover:animate-pulse"
+          icon={faUser}
+          size="2x"
+        />
+      </div>
+      <div>
+        <FontAwesomeIcon
+          id="logoIcon"
+          className="hover:animate-pulse"
+          icon={faSquareCaretDown}
+          size="2x"
+        />
+      </div>
+    </div>
+  );
 }
