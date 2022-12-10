9cdeb557d922be6380aed54c3ee869cb18e2038e
ToniGrbic <tonigrbic.5@gmail.com>
2022-12-10 16:52:43 +0100

    popravljen bug kod text div-a

--- juicy/src/components/dashboard/Carousel.js
+++ juicy/src/components/dashboard/Carousel.js
@@ -17,51 +17,57 @@ const settings = {
 };
 
 const Carousel = () => {
-  const [Users, setUsers] = useState([])
+  const [Users, setUsers] = useState([]);
   useEffect(() => {
     axios
       .get("https://dummyjson.com/users?limit=10&select=firstName,lastName,age,image")
       .then((res) => {
-        const users = res.data.users
-        setUsers(users)
+        const users = res.data.users;
+        setUsers(users);
       });
   }, []);
-  
-  return (
-  <div>
 
-    <Slider {...settings}>
-      { Users.map((user)=>{
-        const { id, firstName, lastName, age, image } = user
-        return (
-          <div
-            key={id}
-            className="userDiv flex-col justify-end items-center w-full mt-8 mb-4 h-full"
-          >
-            <div className="border-4 border-green-300 mb-4 rounded-full h-[10rem] overflow-hidden mx-4 bg-slate-200">
-              <img src={image} className="object-cover h-[10rem]" alt="user"/>
+  return (
+    <div>
+      <Slider {...settings}>
+        {Users.map((user) => {
+          const { id, firstName, lastName, age, image } = user;
+          return (
+            <div
+              key={id}
+              className="userDiv flex-col justify-end items-center w-full mt-8 mb-4 h-full"
+            >
+              <div className="border-4 border-green-300 mb-4 rounded-full h-[10rem] overflow-hidden mx-4 bg-slate-200">
+                <img
+                  src={image}
+                  className="object-cover h-[10rem]"
+                  alt="user"
+                />
+              </div>
+              <div
+                className="flex flex-col mb-8 h-auto max-h-[15rem] overflow-y-auto py-4 items-center
+                           rounded-2xl bg-slate-200 w-3/4 "
+              >
+                <p className="font-bold mt-2 text-lg">
+                  {firstName} {lastName}
+                </p>
+                <p className="text-lg">
+                  <span className="font-bold">Age:</span> {age}
+                </p>
+                <p className="font-bold text-lg">Description:</p>
+                <p className="text-center w-[90%]">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                  ut interdum ex, in iaculis dui. Integer eget orci elit.
+                  Pellentesque sit amet elit sit amet dui interdum laoreet. In
+                  lobortis volutpat erat a dapibus.
+                </p>
+              </div>
             </div>
-            <div className="flex flex-col justify-center mb-8 max-h-[18rem] overflow-y-auto pt-4 pb-2 items-center rounded-2xl bg-slate-200 w-3/4 ">
-              <p className="font-bold mt-2 text-lg">
-                {firstName} {lastName}
-              </p>
-              <p className="text-lg">
-                <span className="font-bold">Age:</span> {age}
-              </p>
-              <p className="font-bold text-lg">Description:</p>
-              <p className="text-center w-[90%]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut
-                interdum ex, in iaculis dui. Integer eget orci elit.
-                Pellentesque sit amet elit sit amet dui interdum laoreet. In
-                lobortis volutpat erat a dapibus.
-              </p>
-            </div>
-          </div>
-        );
-      })}
-    </Slider>
-  </div>
-  )
+          );
+        })}
+      </Slider>
+    </div>
+  );
 };
 
 export default Carousel;
