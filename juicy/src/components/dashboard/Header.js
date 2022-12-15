00d636643ed258b169e192463b673858f6514700
Lepfner <andrija@blissproductions.io>
2022-12-15 18:16:59 +0100

    Settings redesign

--- juicy/src/components/dashboard/Header.js
+++ juicy/src/components/dashboard/Header.js
@@ -16,7 +16,7 @@ export default function Header() {
     }
 
     return (
-        <div className="flex justify-between bg-orange-500 pt-7 pb-7">
+        <div className="flex justify-between bg-skin-primary pt-7 pb-7">
             <div className='ml-10'>
                 <button onClick={() => navMain()}><img src={JuicyLogo} alt='' className="h-20"/></button>
             </div>
--- juicy/src/components/dashboard/Settings.js
+++ juicy/src/components/dashboard/Settings.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ColorPicker from "../dashboard/colorPicker"
 import { getRGBColor, getAccessibleColor } from "../dashboard/utils"
 
@@ -7,76 +7,64 @@ export default function Settings() {
   const primaryColor = getRGBColor(color, "primary")
   const a11yColor = getRGBColor(getAccessibleColor(color), "a11y")
 
-  const presetColors = [
-    "#ef6c00",
-    "#6231af",
-    "#db2777",
-    "#2dd4bf",
-    "#06b6d4",
-    "#10b981",
-  ]
+  const presetColors = ["#ef6c00", "#6231af", "#db2777", "#2dd4bf", "#06b6d4", "#10b981"]
 
   const [isOn, setIsOn] = React.useState(false);
 
-    return (
-      <>
-      
-       
-        <style>:root {`{${primaryColor} ${a11yColor}}`}</style>
-      
-          <div className=' w-full flex justify-center items-center mt-10'>
-            <div className='bg-skin-primary w-3/4  p-3 text-center flex items-center space-x-4 shadow-xl flex-col'>
-            <div>
-            <button className="lg:text-3xl mb-2 md: text-2xl sm: text-xl" onClick={() => setIsOn((prevState) => 
-            !prevState)}>Notifications</button>
-            <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-black checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-            type="checkbox"
-            checked={isOn}
-            onClick={(e) => setIsOn(e.target.val)}/>
+  return (
+    <>
+      <style>:root {`{${primaryColor} ${a11yColor}}`}</style>
+      <div className=' w-full flex justify-center items-center mt-10'>
+        <div className='bg-skin-primary w-3/4 p-3 rounded-2xl text-center flex items-center space-x-4 shadow-xl flex-col sm:flex-row'>
+          <div className='w-5/12 flex flex-col justify-center items-baseline border-r text-white'>
+            <div className='mt-8'>
+              <h1 className="inline lg:text-3xl mb-2 md:text-2xl sm:text-xl">Notifications:</h1>
+              <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-black transition duration-200 cursor-pointer ml-4"
+                type="checkbox"
+                checked={isOn}
+                onClick={(e) => setIsOn(e.target.val)} />
             </div>
-
-
-              
-            
-              
-                <div className="bg-skin-primary text-white m-10">REPORT:</div>
-              <textarea
-              className="block p-2.5 w-1/2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Feel free to let us know what is bothering you and write as many details as you like:"/>
-              <button
-              className="block bg-orange-500 px-4 rounded-md p-2 mt-4 text-white hover:bg-orange-600
-                        lg:text-xl md:text-lg sm: text-lg flex justify-center">Send</button>
-              <div className=" bottom-5 sm:bottom-14 rounded-xl bg-white p-3 text-center flex items-center space-x-4 shadow-xl m-10">
-          <ColorPicker
-            color={color}
-            onChange={setColor}
-            presetColors={presetColors}
-          /> <p className="text-sm font-medium text-gray-500">Custom Color</p></div>
-            <div className='m-10 flex-col relative w-full flex justify-center items-center bg-skin-primary text-white text-center'>
-            <div>ABOUT US:</div>
-                <div>
-                Created by a team of several software developers and testers,
-                 Juicy is an online dating website that allows you meet people around the globe with just a click of a button.
-                  Meet new friends, partners & etc. using our fully responsive & simple to use design.
-                   We hope you have a wonderful time using our website and be sure to let us know
-                    if there are any problems or if something/someone is bothering you.
-
-                  Our team is working around the clock to make sure your experience is smooth as possible.
-                  You can contact us using email or using the website's report system.
-                </div>
-                <div>Meet the team:</div>
-                Toni Grbić,
-                Bruno Ivanković,
-                Jozo Krstanović,
-                Andrija Lerner,
-                Mateo Papuga,
-                Toni Radman
-                </div>
-                <div>Email: lerner.andi@gmail.com</div>
+            <h1 className="bg-skin-primary text-3xl mt-10 mb-4">REPORT:</h1>
+            <textarea
+              className="resize-none focus:outline-none block p-2.5 w-5/6 h-32 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Feel free to let us know what is bothering you and write as many details as you like:" />
+            <button
+              className="block bg-gray-700 px-4 w-32 rounded-md p-2 mt-4 text-white hover:bg-orange-600 lg:text-xl md:text-lg sm: text-lg flex justify-center">
+                Send
+            </button>
+            <h1 className="bg-skin-primary text-3xl mt-8">Page Color:</h1>
+            <div className=" bottom-5 sm:bottom-14 rounded-xl bg-white p-3 text-center flex items-center space-x-4 shadow-xl m-10">
+              <ColorPicker
+                color={color}
+                onChange={setColor}
+                presetColors={presetColors}
+              />
+              <p className="text-sm font-medium text-gray-500">Custom Color</p>
             </div>
-            
           </div>
-         
-      </>
-    )
-  }
+          <div className='m-10 w-2/3 flex flex-col justify-center bg-skin-primary text-white text-center'>
+            <div className='text-3xl'>ABOUT US:</div>
+            <div className='text-lg'>
+              Created by a team of several software developers and testers,<br/>
+              Juicy is an online dating website that allows you meet people around the globe<br/>
+              with just a click of a button. Meet new friends, partners & etc. using our fully responsive<br/>
+              & simple to use design. We hope you have a wonderful time using our website and be sure<br/>
+               to let us know
+              if there are any problems or if something/someone is bothering you.<br/>
+              Our team is working around the clock to make sure your experience is smooth as possible.<br/>
+              You can contact us using email or using the website's report system.
+            </div>
+            <div className='mt-12 text-lg'>Meet the team:
+            Toni Grbić,<br/>
+            Bruno Ivanković,<br/>
+            Jozo Krstanović,<br/>
+            Andrija Lerner,<br/>
+            Mateo Papuga,<br/>
+            Toni Radman<br/></div>
+            <div className='mt-4'>Email contact: lerner.andi@gmail.com</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
