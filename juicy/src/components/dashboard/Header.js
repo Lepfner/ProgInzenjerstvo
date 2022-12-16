abc2fbcba2fe4930d1521ef298d1b8884d1c897c
Bruno Ivankovic <brunoivankovic2001@gmail.com>
2022-12-16 19:35:38 +0100

    logo radi kad refresha se

--- juicy/src/components/dashboard/Header.js
+++ juicy/src/components/dashboard/Header.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSignOut, faGear } from '@fortawesome/free-solid-svg-icons'
 import JuicyLogo from '../images/logo.png';
+import BlackLogo from '../images/blackLogo.png'
 
 export default function Header({a11yColor, primaryColor}) {
 
+
+    
     function navSettings(){
         localStorage.setItem("current", "settings");
         window.location.reload(false);
@@ -18,7 +21,23 @@ export default function Header({a11yColor, primaryColor}) {
     return (
         <div className="flex justify-between bg-skin-primary pt-7 pb-7">
             <div className='ml-10'>
-                <button onClick={() => navMain()}><img src={JuicyLogo} alt='' className="h-20"/></button>
+                <button onClick={() => navMain()}>
+                {(() => {
+        if (a11yColor==="--color-a11y: 255, 255, 255;") {
+          return (
+            <img src={JuicyLogo} alt='' className="h-20"/>
+          )
+        } else {
+          return (
+            <img src={BlackLogo} alt='' className="h-20"/>
+          )
+        }
+      })()}
+                    
+                    
+                    
+                    
+                    </button>
             </div>
             <div className="flex">
                 <button onClick={() => navSettings()}><FontAwesomeIcon id="settingsIcon" className='mr-14 hover:animate-spin text-skin-a11y' icon={faGear} size="2x"/></button>
