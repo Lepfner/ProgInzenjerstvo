2091e3708eab21616318f4b46df00db7da890475
ToniGrbic <tonigrbic.5@gmail.com>
2022-12-18 20:48:53 +0100

    manji ispravci i dodan state

--- juicy/src/components/dashboard/MyProfilePage.js
+++ juicy/src/components/dashboard/MyProfilePage.js
@@ -1,65 +1,82 @@
-import React from "react";
+import React, { useState} from "react";
 import Header from "./Header"
 import Location from "../images/Location.png"
 import logo from "../images/logo.png"
 import Chip from "@mui/material/Chip"
 
-const likes = ["Web Dev", "Nature", "Hiking", "Cycling", "Tehnology", "Good Company", "Interesting conversations","Gaming", "Learning", "Exercising"]
-const dislikes = ["University", "Big social gatherings", "Small Talk", "Crowded places", "Wasting time"]
+const likesArr = ["Web Dev", "Nature", "Hiking", "Cycling", "Tehnology", "Good Company", "Interesting conversations","Gaming", "Learning", "Exercising"]
+const dislikesArr = ["University", "Big social gatherings", "Small Talk", "Crowded places", "Wasting time"]
+const user = {
+  firstName: "Toni",
+  lastName: "Grbić",
+  gender: "Male",
+  age: 21,
+  location: "Split, Croatia",
+  dateOfBirth: "31. 08. 2001.",
+  nationality: "Croatian",
+  status: "looking for friendships",
+  religion:"Agnostic"
+};
+
 const MyProfilePage = () => {
+  const [likes, setLikes] = useState(likesArr)
+  const [dislikes, setDislikes] = useState(dislikesArr)
+  const [userData, setUserData] = useState(user)
+
+  const {firstName, lastName, gender, age, location, dateOfBirth, nationality, status, religion} = userData
   return (
     <>
       <Header />
-      <div className="w-full h-full  flex justify-center mt-12">
+      <div className="w-full h-full  flex justify-center mt-4">
         <div className="min-h-[47rem] max-w-[910px] w-[80%] max-sm:w-[90%] shadow-2xl bg-orange-500 rounded-r-[5rem] flex justify-start items-center">
-          <div className="h-[96%] m-4 pb-8 w-[94%] shadow-2xl bg-slate-200 rounded-r-[4rem] flex flex-col gap-4 max-sm:w-[90%]">
+          <div className="h-[96%] mx-4 my-2 pb-4 w-[94%] shadow-2xl bg-slate-200 rounded-r-[4rem] flex flex-col gap-4 max-sm:w-[90%]">
             <div className="w-full min-h-[13rem] flex gap-4 mx-4 mt-8">
               <section className="w-[90%] h-full bg-slate-300 rounded-xl flex justify-start py-4 max-sm:flex-col max-sm:items-center">
                 <div className="w-[36%] flex flex-col items-center ml-8 pr-4 border-r-2 border-orange-500 max-sm:border-b-2 max-sm:border-none max-sm:w-[90%] max-sm:mb-4">
-                  <div className=" mt-4 mb-2 rounded-full flex justify-center h-[8rem] max-sm:h-[6rem] w-[8rem] max-sm:w-[6rem] overflow-hidden mx-4 bg-slate-200 ">
+                  <div className=" mt-4 mb-2 rounded-full flex justify-center h-[8rem] w-[8rem] overflow-hidden mx-4 bg-slate-200 ">
                     <img
                       src={logo}
-                      className="object-cover h-[8rem] max-sm:h-[6rem]"
+                      className="object-cover h-[8rem]"
                       alt="user"
                     />
                   </div>
-                  <p className="">Toni Grbić, 21</p>
+                  <p className="">
+                    {firstName} {lastName}, {age}
+                  </p>
 
                   <p>
                     <img
                       src={Location}
                       className="w-8 h-8 inline-block mix-blend-color-burn"
                     />
-                    Split, Croatia
+                    {location}
                   </p>
                 </div>
-                <div className="w-[63%] pl-8 pt-2 text-lg max-sm:border-t-2 max-sm:border-orange-500 max-sm:w-[90%]">
+                <div className="w-[63%] pl-8 pt-2 pb-4 text-lg max-sm:border-t-2 max-sm:border-orange-500 max-sm:w-[90%]">
                   <h3>
-                    <span className="font-bold">Date of Birth:</span> 31. 08.
-                    2001.
+                    <span className="font-bold">Date of Birth:</span>
+                    <span className="inline-block"> {dateOfBirth}</span>
                   </h3>
                   <h3>
-                    <span className="font-bold">Nationality:</span> Croatian
+                    <span className="font-bold">Gender:</span>{" "}
+                    {gender}
                   </h3>
                   <h3>
-                    <span className="font-bold">Status:</span> looking for
-                    friendships
+                    <span className="font-bold">Nationality:</span>{" "}
+                    {nationality}
                   </h3>
                   <h3>
-                    <span className="font-bold">Work:</span> Student
+                    <span className="font-bold">Status:</span> {status}
                   </h3>
                   <h3>
-                    <span className="font-bold">Education:</span> CS major
-                  </h3>
-                  <h3>
-                    <span className="font-bold">Religion:</span> Agnostic
+                    <span className="font-bold">Religion:</span> {religion}
                   </h3>
                 </div>
               </section>
             </div>
-            <div className="w-[90%] min-h-[13rem] bg-slate-300 mx-4 rounded-xl p-4">
+            <section className="w-[90%] min-h-[13rem] bg-slate-300 mx-4 rounded-xl py-4 px-8">
               <h2 className="text-center text-2xl">About</h2>
-              <p>
+              <p className="text-lg">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
                 non porta eros. Donec eget diam at ante lacinia cursus.
                 Phasellus dignissim tortor nibh, at accumsan sem suscipit vitae.
@@ -68,8 +85,8 @@ const MyProfilePage = () => {
                 augue, eget feugiat nunc diam ut diam. Ut non elit nec magna
                 molestie gravida ac vitae velit.
               </p>
-            </div>
-            <div className="w-[90%] min-h-[13rem] bg-slate-300 mx-4 rounded-xl p-4">
+            </section>
+            <section className="w-[90%] min-h-[13rem] bg-slate-300 mx-4 rounded-xl px-8 py-4">
               <h2 className="text-center text-2xl">Likes & Dislikes</h2>
               <h3 className="text-lg">Likes:</h3>
 
@@ -94,7 +111,7 @@ const MyProfilePage = () => {
                   />
                 );
               })}
-            </div>
+            </section>
           </div>
         </div>
       </div>
