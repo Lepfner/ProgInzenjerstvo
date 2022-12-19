6537d27cda31ecdc7ad53a94c568499c612fbc89
ToniGrbic <tonigrbic.5@gmail.com>
2022-12-19 14:04:08 +0100

    fixed filter responsivness and centered carousel

--- juicy/src/components/dashboard/MainMenu.js
+++ juicy/src/components/dashboard/MainMenu.js
@@ -9,7 +9,7 @@ export default function MainMenu({currentPage}) {
     return (
         <div className='h-screen w-full'>
             <Header />
-            <div className='h-5/6 w-full flex'>
+            <div className='h-5/6 w-full flex justify-center'>
                 <div className="h-full lg:w-full md: w-10/12">
                     {currentPage==="settings" ? <Settings/> : currentPage==="main" ? <Content/> : <Content/>}
                 </div>
