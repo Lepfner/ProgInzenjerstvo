749b11e1358f7c031f59b42584587c432d91649f
ToniGrbic <tonigrbic.5@gmail.com>
2022-12-30 17:50:58 +0100

    added db_username env var

--- server/config/database.js
+++ server/config/database.js
@@ -2,5 +2,5 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 module.exports = new Sequelize(
-  `postgresql://toni_grbic:${process.env.DB_PASS}@juicydb-6413.8nj.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full`
+  `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASS}@juicydb-6413.8nj.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full`
 );
