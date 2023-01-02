4b56395acaaaf739a8c1cf6f7a68966388e5682a
ToniGrbic <tonigrbic.5@gmail.com>
2023-01-02 22:55:52 +0100

    promjenjen string za konekciju u bazu sa shemama

--- server/config/database.js
+++ server/config/database.js
@@ -2,5 +2,5 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 module.exports = new Sequelize(
-  `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASS}@juicydb-6413.8nj.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full`
+  `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASS}@juicydb-6413.8nj.cockroachlabs.cloud:26257/Juicy?sslmode=verify-full`
 );
