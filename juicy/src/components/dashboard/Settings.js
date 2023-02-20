9e7da63d59924b2cd4b11f084be90f92d349b2eb
Toni Radman <toniradman2903@gmail.com>
2023-02-20 15:02:01 +0100

    Report problem fix(user + message cleanup)

--- juicy/src/components/dashboard/Settings.js
+++ juicy/src/components/dashboard/Settings.js
@@ -4,8 +4,11 @@ import { getRGBColor, getAccessibleColor } from "../dashboard/utils"
 import Header from '../dashboard/Header';
 import { useNavigate } from "react-router-dom";
 import emailjs from '@emailjs/browser';
+import { toast } from 'react-hot-toast';
 
 export default function Settings() {
+  const [user, setUser] = useState("");
+  const [message, setMessage] = useState("");
   const [color, setColor] = useState(localStorage.getItem("currentColor"));
   const primaryColor = getRGBColor(color, "primary");
   const a11yColor = getRGBColor(getAccessibleColor(color), "a11y");
@@ -13,6 +16,9 @@ export default function Settings() {
   const navigate = useNavigate();
 
   const presetColors = ["#ef6c00", "#6231af", "#db2777", "#2dd4bf", "#06b6d4", "#10b981"];
+
+  const onInputUser = (e) => setUser(e.target.value);
+  const onInputMessage = (e) => setMessage(e.target.value);
 
   function checkUserToken() {
     if (localStorage.getItem("isLoggedIn") === 'false') {
@@ -29,11 +35,13 @@ export default function Settings() {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_l72z64l', 'template_mu6s50h', form.current, 'M-karua9jmM9OyLKr')
+    emailjs.sendForm('service_btrio0p', 'template_1n7duvu', form.current, 'xnjBvyWRO-Ew4a6Qz')
       .then((result) => {
-        // show the user a success message
+        toast.success("Report sent!");
+        setUser("");
+        setMessage("");
       }, (error) => {
-        console.log(error);
+        console.log(error); 
       });
   };
 
@@ -46,11 +54,13 @@ export default function Settings() {
           <div className='md:w-5/12 w-full flex flex-col justify-center items-center md:border-r border-0 text-skin-a11y'>
             <h1 className="bg-skin-primary text-3xl mt-10 mb-4">Report a problem:</h1>
             <form ref={form} onSubmit={sendEmail} className='w-full'>
-              <input type='text' name="user" placeholder='Write your username'
+              <input type='text' name="user" value={user} onInput={onInputUser} placeholder='Write your username'
                 className="resize-none focus:outline-none p-2.5 w-5/6 h-10 mb-6 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               </input>
               <textarea
                 name="message"
+                value={message}
+                onInput={onInputMessage}
                 className="resize-none focus:outline-none p-2.5 w-5/6 h-32 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Feel free to let us know what is bothering you and write as many details as you like:" />
               <button
