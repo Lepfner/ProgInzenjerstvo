d9a86ad9829443de440f06d26844c8a29c0f3709
Toni Radman <toniradman2903@gmail.com>
2023-02-20 16:03:59 +0100

    2FA mail change

--- server/verification/transporter.js
+++ server/verification/transporter.js
@@ -4,8 +4,8 @@ let transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   auth: {
-    user: "tonigrbic.5@gmail.com",
-    pass: "mhdrwllrouvovgpw",
+    user: "toniradman2903@gmail.com",
+    pass: "drucehwflrqsxgha",
   },
 });
 
